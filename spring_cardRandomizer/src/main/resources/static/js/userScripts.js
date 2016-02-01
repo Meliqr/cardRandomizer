767c00014113ed4f13325a5717768e98386c3d5f
Elliot Kindt <bakuchi@yahoo.com>
2016-02-01 13:43:26 -0500

    Open Booster is now based upon the set selected in the dropdown

--- spring_cardRandomizer/src/main/resources/static/js/userScripts.js
+++ spring_cardRandomizer/src/main/resources/static/js/userScripts.js
@@ -52,33 +52,91 @@ cardRandomizer.controller('userRegisterCtrl', function($scope, $http) {
 });
 
 cardRandomizer.controller('openBoosterCtrl', function($scope, $http) {
-    $scope.boosterReturn = "";
+    $scope.boosterReturn = [];
     $scope.allSets = "";
-    $http.get('cards/setInfo.json').success(function(data) {
+    $scope.cardSetName = "Magic Origins";
+    $scope.setCards = {};
+    $scope.cardSet = "ORI";
+
+    $http.get('cards/setInfo.json').success(function (data) {
         $scope.allSets = data;
     });
 
-    $scope.cardSet = "ZEN";
+    $http.get("/cards/" + $scope.cardSet + "_cards.json").success(function (data) {
+            $scope.setCards = data;
+    });
+
+
     $scope.openBooster = function() {
+        var cardsReduced = $scope.setCards.map(function(curCard){
+            return curCard.editions.filter(function(curEd){
+             return curEd.set_id == $scope.cardSet;
+            }).map(function(singleEd){
+                return {"name":curCard.name,"url":curCard.url,"set":singleEd.set,"set_id":singleEd.set_id,"rarity":singleEd.rarity,"image_url":singleEd.image_url};
+            });
+        }).reduce(function(pre, cur) {
+            return pre.concat(cur);
+        });
+
+        var commonCards = cardsReduced.filter(function(curCard){
+            return curCard.rarity == "common";
+        });
+        var uncommonCards = cardsReduced.filter(function(curCard){
+            return curCard.rarity == "uncommon";
+        });
+        var rareCards = cardsReduced.filter(function(curCard){
+            return curCard.rarity == "rare";
+        });
+        var mythicCards = cardsReduced.filter(function(curCard){
+            return curCard.rarity == "mythic";
+        });
+        var landCards = cardsReduced.filter(function(curCard){
+            return curCard.rarity == "basic";
+        });
+
         var request = $http({
             method: "post",
             url: "/getBooster",
             data: {
-                "setName": "Zendikar",
-                "numCommon": 101,
-                "setAbbreviation": "ZEN",
-                "numUncommon": 60,
-                "numRare": 53,
-                "numMythic": 15,
-                "numLands": 20,
+                "setName": $scope.cardSetName,
+                "numCommon": commonCards.length,
+                "setAbbreviation": $scope.cardSet,
+                "numUncommon": uncommonCards.length,
+                "numRare": rareCards.length,
+                "numMythic": mythicCards.length,
+                "numLands": landCards.length,
                 "packSize": 15,
                 "hasPremium": true
             }
         });
 
         request.success(function (data) {
-            console.log(data);
-            $scope.boosterReturn = data;
+            $scope.boosterReturn =[];
+            data.cards.forEach(function(curCard){
+                if(curCard.cardId <= commonCards.length){
+                    $scope.boosterReturn.push(commonCards[curCard.cardId-1]);
+                }
+                else if(curCard.cardId <= (commonCards.length + uncommonCards.length)){
+                    $scope.boosterReturn.push(uncommonCards[curCard.cardId-commonCards.length-1]);
+                }
+                else if((curCard.cardId <= (commonCards.length + uncommonCards.length + rareCards.length)) && (rareCards.length)){
+                    $scope.boosterReturn.push(rareCards[curCard.cardId-commonCards.length-uncommonCards.length-1]);
+                }
+                else if((curCard.cardId <= (commonCards.length + uncommonCards.length + rareCards.length +mythicCards.length)) && (mythicCards.length)){
+                    $scope.boosterReturn.push(mythicCards[curCard.cardId-commonCards.length-uncommonCards.length-rareCards.length-1]);
+                }
+                else{
+                    $scope.boosterReturn.push(landCards[curCard.cardId-commonCards.length-uncommonCards.length-rareCards.length-mythicCards.length-1]);
+                }
+            });
         });
     }
+    $scope.getSetCards = function() {
+        var singleSet = $scope.allSets.forEach(function(setCheck){if(setCheck.id == $scope.cardSet){$scope.cardSetName = setCheck.name;}});
+        $http.get("/cards/"+$scope.cardSet+"_cards.json")
+            .success(function(data) {
+                $scope.setCards = data;
+            });
+    }
+
 });
